b654501642e2c8fbd3d1a56ea4136dfaf56fe237
DannyChu <rucvan.pr@gmail.com>
2022-10-12 20:25:03 +0700

    define store and leads model

--- unity/admin.py
+++ unity/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Store, Leads
 
-# Register your models here.
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = [
+        'store_name',
+        'store_owner',
+        'website_url',
+        'city',
+        'country'
+    ]
+    list_filter = ['country',]
+    search_fields = ['store_name', 'website_url']
+
+
+@admin.register(Leads)
+class LeadsAdmin(admin.ModelAdmin):
+    list_display = [
+        'store',
+        'email_address',
+        'status'
+    ]
+    list_filter = ['status','store']
+    search_fields = ['email_address']
--- unity/models.py
+++ unity/models.py
@@ -1,28 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 
 class Store(models.Model):
-    """
-    todo: define Store model
-    - store name
-    - owner
-    - url
-    - address line 1
-    - address line 2
-    - city
-    - state
-    - country
-    """
-    pass
+    store_name = models.CharField(max_length=255)
+    store_owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
+    website_url = models.CharField(max_length=255)
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    country = CountryField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.store_name
+
+    class Meta:
+        ordering = ['store_name']
 
 
-class Lead(models.Model):
-    """
-    todo: define Lead model
-    - store: Store
-    - email address
-    - status <option: subscribed, un-subscribed
-    - date created
-    - date last update
-    """
-    pass
+class Leads(models.Model):
+    # enumeration for status fields
+    class Status(models.TextChoices):
+        SUBSCRIPBED = 'SUB', 'Subscribed'
+        UNSUBSCRIBED = 'UNS', 'Un-subscribed'
+
+    store = models.ForeignKey(
+        to=Store,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    email_address = models.EmailField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=3,
+        choices=Status.choices,
+        default='SUB'
+    )
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+
+    def __str__(self):
+        return self.email_address
